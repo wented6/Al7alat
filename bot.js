f00b96542e74223eb9aa80c36e52efecf6f0f13c
wented6 <44713416+wented6@users.noreply.github.com>
2018-12-07 18:39:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -131,7 +131,30 @@ client.on('ready', () => {
 
 
 
+ client.on('message', msg => {
+	var prefix = "";
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
 
+    if(command === "مثح") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("**ضع عدد الرسائل التي تريد مسحها**").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
 
 
 
